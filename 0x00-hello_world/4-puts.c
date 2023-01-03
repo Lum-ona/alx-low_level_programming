a1b95606e18775ab8cecce84e3c6e912dca5c7ec
Lum-ona <lumonamulengwa@gmail.com>
2023-01-03 15:24:40 +0300

    printing out sth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+* main - printing out a text
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
