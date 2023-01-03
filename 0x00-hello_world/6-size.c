b9b37cc0687ad7a6e5521912ade63fadcc002df9
Lum-ona <lumonamulengwa@gmail.com>
2023-01-03 15:43:08 +0300

    an interesting task, 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+*main - prints various size
+*Return: 0 (success)
+*/
+int main(void)
+{
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+printf("size of a char: %lu byte(s)\n", sizeof(c));
+printf("size of a int: %lu byte(s)\n", sizeof(i));
+printf("size of a long int: %lu byte(s)\n", sizeof(li));
+printf("size of a long long int: %lu byte(s)\n", sizeof(lli));
+printf("size of a float: %lu byte(s)\n", sizeof(f));
+return (0);
+}
